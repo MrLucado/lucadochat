0029e8cab2e882c0a5e3448b5ca6e3e3d5fc2d5d
Jacob Lucado <63121061+MrLucado@users.noreply.github.com>
2022-09-20 17:40:17 -0400

    Update upload.js

--- client/src/utils/upload.js
+++ client/src/utils/upload.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-const proxyURL = 'https://robwu.nl/cors-anywhere.html/';
+//const proxyURL = 'https://robwu.nl/cors-anywhere.html/';
 const uploadURL = 'https://uguu.se/api.php?d=upload-tool';
 
 async function upload(attachment) {
   let formData = new FormData();
   formData.append('file', attachment);
 
-  return await axios.post(proxyURL + uploadURL, formData, {
+  return await axios.post( uploadURL, formData, {
       headers: { 'Content-Type': 'multipart/form-data' },
       crossdomain: true
     })
